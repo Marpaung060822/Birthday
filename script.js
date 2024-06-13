c08d7b53571a3ca73a6151c598069e9db2a07a02
Marpaung060822 <carlosmarpaung062@gmail.com>
2024-06-13 21:32:49 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc");
+const showImageBtn = document.getElementById("showImageBtn");
+const birthdayImage = document.getElementById("birthdayImage");
+const birthdaySong = document.getElementById("birthdaySong");
+
+title.innerText = "Miranda";
+
+desc.innerText =
+  "Happy 20th Birthday!\nMay your special day be filled with joy and happiness.\nI hope you find happiness in the choices you have made and discover all the joy you seek.";
+
+showImageBtn.addEventListener("click", () => {
+  // Show the birthday image
+  birthdayImage.style.display = "block";
+  // Play the birthday song
+  birthdaySong.play();
+});
