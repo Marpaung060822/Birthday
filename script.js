4ae27db8da64d15fd1267578030bbfb74abfe77b
Marpaung060822 <carlosmarpaung062@gmail.com>
2024-06-14 10:23:42 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const birthdaySong = document.getElementById("birthdaySong");
 title.innerText = "Miranda";
 
 desc.innerText =
-  "Happy 20th Birthday!\nMay your special day be filled with joy and happiness.\nI hope you find happiness in the choices you have made and discover all the joy you seek.";
+  "Happy 20th Birthday!\nMay your special day be filled with joy and happiness.\nI hope you find happiness in the CHOICE you have made and discover all the joy you seek.";
 
 showImageBtn.addEventListener("click", () => {
   // Show the birthday image
